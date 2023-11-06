fe1fe8ceb7d39594be1b879e09e3202781f7d57e
thayrov <thayrovg@gmail.com>
2023-11-06 15:53:36 -0600

    complete BinarioADecimal and DecimalABinario exercises

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,10 +1,31 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+let BinarioADecimal = num => parseInt(num, 2);
+/*  
+function BinarioADecimal(num) {
+  let binario = num.split('').reverse();
+  let decimal = binario
+  .map((b, index) => {
+    return 2 ** index * Number(b);
+  })
+  .reduce((acc, curr) => acc + curr);
+  return decimal; 
+} */
 
-function DecimalABinario(num) {}
+let DecimalABinario = num => num.toString(2);
+/*   
+function DecimalABinario(num) {
+  let bin = [];
+  while (num > 0) {
+    bin.unshift(num % 2);
+    num = Math.floor(num / 2);
+  }
+  let res = bin.join('');
+  return res; 
+}
+*/
 
 module.exports = {
-   BinarioADecimal,
-   DecimalABinario,
+  BinarioADecimal,
+  DecimalABinario,
 };
