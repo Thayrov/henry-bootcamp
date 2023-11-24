d8f1ba9442feb366f10143b0357354e41a3a3e10
thayrov <thayrovg@gmail.com>
2023-11-24 15:11:45 -0600

    add new exercises

--- FT-M2-master/06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ FT-M2-master/06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -1,5 +1,11 @@
 import Card from './Card';
 
-export default function Cards(props) {
-   return <div></div>;
+export default function Cards({characters}) {
+  return (
+    <div>
+      {characters.map(character => (
+        <Card key={character.id} {...character} />
+      ))}
+    </div>
+  );
 }
