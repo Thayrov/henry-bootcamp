228a8c60212497f3c6ad77b3822cf03029728990
thayrov <thayrovg@gmail.com>
2023-11-25 09:24:10 -0600

    refactor exercises

--- FT-M2-master/06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ FT-M2-master/06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -4,7 +4,11 @@ export default function Cards({characters}) {
   return (
     <div>
       {characters.map(character => (
-        <Card key={character.id} {...character} />
+        <Card
+          key={character.id}
+          {...character}
+          onClose={() => window.alert('Emulamos que se cierra la card')}
+        />
       ))}
     </div>
   );
