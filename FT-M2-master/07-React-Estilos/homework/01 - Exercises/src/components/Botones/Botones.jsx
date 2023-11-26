4fdb2b88ac9bf38a2a0a14fd9ee308c49c66805f
thayrov <thayrovg@gmail.com>
2023-11-25 23:10:19 -0600

    add new exercises

--- FT-M2-master/07-React-Estilos/homework/01 - Exercises/src/components/Botones/Botones.jsx
+++ FT-M2-master/07-React-Estilos/homework/01 - Exercises/src/components/Botones/Botones.jsx
@@ -1,20 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
 
-
-const DivButtons = 'DivButtons'
-const Buttons = 'Buttons'
+const DivButtons = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  gap: 20px;
+  margin-top: 20px;
+`;
+const Buttons = styled.button`
+  width: auto;
+  height: 50px;
+  border-radius: 5px;
+  margin: 10px;
+  padding: 5px;
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: 600;
+  transition: background-color 0.3s ease, color 0.3s ease;
+  &:hover {
+    color: white;
+    background-color: red;
+  }
+`;
 
 export default class Botones extends React.Component {
-  render () {
-    const { alerts } = this.props
+  render() {
+    const {alerts} = this.props;
     return (
-      <div>
-        <button onClick={() => window.alert(alerts.m1)}>Módulo 1</button>
-        <button onClick={() => window.alert(alerts.m2)}>Módulo 2</button>
-      </div>
-    )
+      <DivButtons>
+        <Buttons onClick={() => window.alert(alerts.m1)}>Módulo 1</Buttons>
+        <Buttons onClick={() => window.alert(alerts.m2)}>Módulo 2</Buttons>
+      </DivButtons>
+    );
   }
 }
 
 // Esto lo exportamos para los tests
-export { DivButtons, Buttons }
+export {DivButtons, Buttons};
