051496ab76a7a810c58848bc9d6712905811c8fa
thayrov <thayrovg@gmail.com>
2023-11-27 16:50:24 -0600

    :sparkles: [ADD] M2 C8 HM1 E3

--- FT-M2-master/08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
+++ FT-M2-master/08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
@@ -2,11 +2,24 @@ import React from 'react';
 // import styledAnimals from './Animals.module.css'
 
 export default class Animals extends React.Component {
+  // eslint-disable-next-line
   constructor(props) {
-    super();
+    super(props);
   }
 
   render() {
-    return <div></div>
+    return (
+      <div>
+        {this.props.propAnimals.map((animal, index) => (
+          <div key={index}>
+            <h5>{animal.name}</h5>
+            <img width='300px' src={animal.image} alt={animal.name} />
+            <br />
+
+            <span>{animal.specie}</span>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
