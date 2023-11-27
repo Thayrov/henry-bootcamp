ba569b00317d329e40eb72ac02a799a1f77d33fe
thayrov <thayrovg@gmail.com>
2023-11-27 13:36:27 -0600

    :sparkles: [ADD] M2 C8 HM1 E1.

--- FT-M2-master/08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
+++ FT-M2-master/08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
@@ -1,17 +1,22 @@
-import React from "react";
-// eslint-disable-next-line no-unused-vars
-import Animals from "../Animals/Animals";
-// eslint-disable-next-line no-unused-vars
-import Species from "../Species/Species";
-// import styledZoo from "./Zoo.module.css";
+import React from 'react';
+import Animals from '../Animals/Animals';
+import Species from '../Species/Species';
+import styledZoo from './Zoo.module.css';
 
 export default function Zoo() {
-  /* Escribe acá tu código */
+  const [zoo, setZoo] = React.useState({
+    zooName: '',
+    animals: [],
+    species: [],
+    allAnimals: [],
+  });
 
-
+  const handleIputChange = e => setZoo({...zoo, zooName: e.target.value});
   return (
     <div>
-      <h1>-</h1>
+      <label>Zoo Name:</label>
+      <input type='text' value={zoo.zooName} onChange={handleIputChange} />
+      <h1>{zoo.zooName}</h1>
     </div>
   );
 }
