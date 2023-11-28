b4140b6669b5065d89d4d61f735e8a3e30b0331d
thayrov <thayrovg@gmail.com>
2023-11-27 19:31:41 -0600

    :sparkles: [ADD] M2 C8 HM1 E4

--- FT-M2-master/08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Species/Species.jsx
+++ FT-M2-master/08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Species/Species.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React from 'react';
 // import styledSpecies from "./Species.module.css";
 
-export default function Species({ species, handleSpecies, handleAllSpecies }) {
+export default function Species({species, handleSpecies, handleAllSpecies}) {
   // console.log(species);
-  return <div></div>
+  return (
+    <div>
+      <h2>Species</h2>
+      {species.map((specie, i) => {
+        return (
+          <button key={i} onClick={handleSpecies} value={specie}>
+            {' '}
+            {specie}
+          </button>
+        );
+      })}
+      <button onClick={handleAllSpecies}>All Animals</button>
+    </div>
+  );
 }
