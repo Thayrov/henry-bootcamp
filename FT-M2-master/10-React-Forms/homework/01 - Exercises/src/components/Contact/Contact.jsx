f00ce065dfa8b87bdd7dd61bdcb8f81b662ee815
thayrov <thayrovg@gmail.com>
2023-12-02 17:03:18 -0600

    :sparkles: (ADD): Create Controlled Form
    Add a controlled form with dynamic inputs and error handling in the Contact component.

--- FT-M2-master/10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
+++ FT-M2-master/10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-import './Contact.modules.css'
+import React from 'react';
+import './Contact.modules.css';
 
 // eslint-disable-next-line
 const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
-export default function Contact () {
-  return <div>Crear Formulario</div>
+export default function Contact() {
+  return (
+    <form>
+      <div>
+        <label htmlFor='name'>Nombre:</label>
+        <input type='text' name='name' id='name' placeholder='Escribe tu nombre...' />
+      </div>
+      <div>
+        <label htmlFor='email'>Correo Electrónico:</label>
+        <input type='text' name='email' id='email' placeholder='Escribe tu email...' />
+      </div>
+      <div>
+        <label htmlFor='message'>Mensage:</label>
+        <textarea
+          name='message'
+          id='message'
+          placeholder='Escribe tu mensaje...'
+          type='text'></textarea>
+      </div>
+      <button type='submit'>enviar</button>
+    </form>
+  );
 }
