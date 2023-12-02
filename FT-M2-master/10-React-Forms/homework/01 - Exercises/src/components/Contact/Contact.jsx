465921df633a1bc0aababa4d09dee7b95e25ca11
thayrov <thayrovg@gmail.com>
2023-12-02 17:49:22 -0600

    :sparkles: (ADD): Manage Input State
    Implement state hooks for input values and errors, and connect them to form inputs.

--- FT-M2-master/10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
+++ FT-M2-master/10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
@@ -5,15 +5,46 @@ import './Contact.modules.css';
 const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
 export default function Contact() {
+  const [inputs, setInputs] = React.useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const [errors, setErrors] = React.useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = e => {
+    const {name, value} = e.target;
+    setInputs(prev => ({...prev, [name]: value}));
+  };
+
   return (
     <form>
       <div>
         <label htmlFor='name'>Nombre:</label>
-        <input type='text' name='name' id='name' placeholder='Escribe tu nombre...' />
+        <input
+          type='text'
+          name='name'
+          id='name'
+          placeholder='Escribe tu nombre...'
+          value={inputs.name}
+          onChange={handleChange}
+        />
       </div>
       <div>
         <label htmlFor='email'>Correo Electrónico:</label>
-        <input type='text' name='email' id='email' placeholder='Escribe tu email...' />
+        <input
+          type='text'
+          name='email'
+          id='email'
+          placeholder='Escribe tu email...'
+          value={inputs.email}
+          onChange={handleChange}
+        />
       </div>
       <div>
         <label htmlFor='message'>Mensage:</label>
@@ -21,7 +52,9 @@ export default function Contact() {
           name='message'
           id='message'
           placeholder='Escribe tu mensaje...'
-          type='text'></textarea>
+          type='text'
+          value={inputs.message}
+          onChange={handleChange}></textarea>
       </div>
       <button type='submit'>enviar</button>
     </form>
