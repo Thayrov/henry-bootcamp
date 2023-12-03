db633923768059928ce46b1a71f95c7e80d9652c
thayrov <thayrovg@gmail.com>
2023-12-02 21:40:28 -0600

    :sparkles: (ADD): Setup Reducer and Create Actions
    Create a reducer to manage the global state of the counter application. Develop distinct actions to send information to the reducer for state management.

--- FT-M2-master/11-Redux/homework/01 - Exercises/src/actions/index.js
+++ FT-M2-master/11-Redux/homework/01 - Exercises/src/actions/index.js
@@ -1,15 +1,27 @@
-const { INCREMENTO, DECREMENTO } = require('../action-types');
+const {INCREMENTO, DECREMENTO} = require('../action-types');
 
-// Nuestras actions (action creators) devolverán un paquete de actions que nuestro reducer recibirá. 
-// ¿Cómo es el paquete de acción? Tengan en cuenta que el creador de la acción no es en absoluto responsable 
+// Nuestras actions (action creators) devolverán un paquete de actions que nuestro reducer recibirá.
+// ¿Cómo es el paquete de acción? Tengan en cuenta que el creador de la acción no es en absoluto responsable
 // de manejar ninguna de las lógicas actuales de actualización del store central de Redux.
 // Eso se lo deja al reducer(s).
 
-const incremento = null;
+// Action creator para incrementar el contador
+const incremento = () => {
+  return {
+    type: INCREMENTO,
+    // Aquí no hay payload porque la información necesaria para incrementar ya está implícita
+  };
+};
 
-const decremento = null;
+// Action creator para decrementar el contador
+const decremento = () => {
+  return {
+    type: DECREMENTO,
+    // Aquí no hay payload porque la información necesaria para decrementar ya está implícita
+  };
+};
 
 module.exports = {
   incremento,
-  decremento
-}
+  decremento,
+};
