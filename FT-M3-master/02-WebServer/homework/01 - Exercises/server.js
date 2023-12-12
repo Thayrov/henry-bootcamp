3edc4e8c90a17dc3ae3a8490000aa4115aa1b3cf
thayrov <thayrovg@gmail.com>
2023-12-12 12:55:30 -0600

    💡 [ADD]: HTML Endpoint Addition
    Added '/allDogs' endpoint in server.js to serve allDogs.html content, with error handling as per HW Exercise 3.

--- FT-M3-master/02-WebServer/homework/01 - Exercises/server.js
+++ FT-M3-master/02-WebServer/homework/01 - Exercises/server.js
@@ -18,6 +18,18 @@ const server = http
       });
       return;
     }
+    if (req.url === '/allDogs') {
+      fs.readFile('./utils/allDogs.html', 'UTF8', (err, data) => {
+        if (err) {
+          res.writeHead(404, {'Content-Type': 'text/plain'});
+          res.end('html not found');
+        } else {
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.end(data);
+        }
+      });
+      return;
+    }
   })
   .listen(PORT, 'localhost');
 /* ⚠️ LA LÍNEA SIGUIENTE TIENE QUE QUEDAR COMO ESTÁ PARA PODER EXPORTAR EL SERVIDOR ⚠️ */
