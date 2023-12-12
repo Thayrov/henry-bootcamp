0bbb508ec62968e9d90994b80f048df06f1c9dfe
thayrov <thayrovg@gmail.com>
2023-12-12 11:36:00 -0600

    :sparkles: (ADD): Initialize Server Setup
    Configured PORT and basic server setup in server.js as per HW Exercise 1 requirements.

--- FT-M3-master/02-WebServer/homework/01 - Exercises/server.js
+++ FT-M3-master/02-WebServer/homework/01 - Exercises/server.js
@@ -1,10 +1,14 @@
-var fs = require("fs");
-var http = require("http");
+var fs = require('fs');
+var http = require('http');
 /* ⚠️ NO MODIFICAR NADA POR ENCIMA DE ESTA LÍNEA ⚠️ */
 /* AQUÍ DEBAJO PUEDES ESCRIBIR LA CONSTANTE DEL PUERTO */
-const PORT = null;
-
+const PORT = 3001;
+const server = http
+  .createServer((req, res) => {
+    console.log(`Server raised in port ${PORT}`);
+  })
+  .listen(PORT, 'localhost');
 /* ⚠️ LA LÍNEA SIGUIENTE TIENE QUE QUEDAR COMO ESTÁ PARA PODER EXPORTAR EL SERVIDOR ⚠️ */
 module.exports =
   /* AQUÍ DEBAJO YA PUEDES ESCRIBIR TÚ CÓDIGO REEMPLAZANDO EL VALOR DE NULL POR EL SERVIDOR */
-  null;
+  server;
