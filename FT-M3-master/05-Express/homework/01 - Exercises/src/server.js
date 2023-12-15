d87d9ab5487d5f064e8d029ac0a89373bde56158
thayrov <thayrovg@gmail.com>
2023-12-15 17:50:27 -0600

    💡 [ADD]: Initialize API Structure
    Added basic structure for CRUD API and initialized in-memory array to store posts.

--- FT-M3-master/05-Express/homework/01 - Exercises/src/server.js
+++ FT-M3-master/05-Express/homework/01 - Exercises/src/server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 
 let publications = [];
 
@@ -6,5 +6,24 @@ const server = express();
 
 server.use(express.json());
 
+server.post('/posts', (req, res) => {
+  const {author, title, contents} = req.body;
+  if (author && title && contents) {
+    const newPost = {
+      id: Date.now(),
+      author,
+      title,
+      contents,
+    };
+    publications.push(newPost);
+    res.status(201).json(newPost);
+  } else {
+    const errorObject = {
+      error: 'No se recibieron los parámetros necesarios para crear la publicación',
+    };
+    res.status(400).json(errorObject);
+  }
+});
+
 //NO MODIFICAR EL CODIGO DE ABAJO. SE USA PARA EXPORTAR EL SERVIDOR Y CORRER LOS TESTS
-module.exports = { publications, server };
+module.exports = {publications, server};
