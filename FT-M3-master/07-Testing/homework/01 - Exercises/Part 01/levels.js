19c3bd5ef7d2d05e1fab3c0a6a0c05f1d0fc9542
thayrov <thayrovg@gmail.com>
2024-01-08 16:17:56 -0600

    💡 [ADD]: Implement Exercises
    Added basic solutions for the problems as per homework requirements.

--- FT-M3-master/07-Testing/homework/01 - Exercises/Part 01/levels.js
+++ FT-M3-master/07-Testing/homework/01 - Exercises/Part 01/levels.js
@@ -1,9 +1,29 @@
-const levelOne = (a, b) => {};
+const levelOne = (a, b) => {
+  return a + b;
+};
 
-const levelTwo = (letras) => {};
+const levelTwo = letras => {
+  if (letras.length < 2) return letras;
+  if (letras.length === 2) return letras[0];
+  let letrasImpar = [];
+  for (let i = 0; i < letras.length; i += 2) {
+    letrasImpar.push(letras[i]);
+  }
+  return letrasImpar.join('');
+};
 
-const levelThree = (a, b) => {};
+const levelThree = (a, b) => {
+  return [...a, ...b].sort((a, b) => a - b);
+};
 
-const levelFour = (num) => {};
+const levelFour = num => {
+  let sum = num
+    .toString()
+    .split('')
+    .map(Number)
+    .reduce((a, b) => a + b);
+  let reverse = parseInt(sum.toString().split('').reverse().join(''));
+  return sum * reverse === num;
+};
 
-module.exports = { levelOne, levelTwo, levelThree, levelFour };
+module.exports = {levelOne, levelTwo, levelThree, levelFour};
--- FT-M3-master/07-Testing/homework/01 - Exercises/Part 02/antiTrollsSecurity.js
+++ FT-M3-master/07-Testing/homework/01 - Exercises/Part 02/antiTrollsSecurity.js
@@ -1,3 +1,22 @@
-const antiTrollsSecurity = (string) => {};
+const antiTrollsSecurity = string => {
+  let newString = '';
+  for (let i = 0; i < string.length; i++) {
+    if (
+      string[i] !== 'a' &&
+      string[i] !== 'e' &&
+      string[i] !== 'i' &&
+      string[i] !== 'o' &&
+      string[i] !== 'u' &&
+      string[i] !== 'A' &&
+      string[i] !== 'E' &&
+      string[i] !== 'I' &&
+      string[i] !== 'O' &&
+      string[i] !== 'U'
+    ) {
+      newString += string[i];
+    }
+  }
+  return newString;
+};
 
 module.exports = antiTrollsSecurity;
