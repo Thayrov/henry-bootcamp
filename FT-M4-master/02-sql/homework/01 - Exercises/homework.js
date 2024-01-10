67814090f7026247ebb048b30a450a0a113c6a0e
thayrov <thayrovg@gmail.com>
2024-01-09 20:32:51 -0600

    💡 [ADD]: Complete Movie DB Queries
    Implemented various SQL queries in homework.js for fetching specific movie data from the 'movies' database, including connections, short films, 1930s films, specific title searches, single actor movies, average ratings, and actors from 'Fast and' movies of 2016.

--- FT-M4-master/02-sql/homework/01 - Exercises/homework.js
+++ FT-M4-master/02-sql/homework/01 - Exercises/homework.js
@@ -1,22 +1,23 @@
 /* ¡Escribe tus comandos en este archivo! */
 
-const ejercicio02 = '';
+const ejercicio02 = 'SELECT * FROM movies WHERE duration < 90;';
 
-const ejercicio03 = '';
+const ejercicio03 = 'SELECT * FROM movies WHERE year BETWEEN 1930 AND 1940;';
 
-const ejercicio04 = '';
+const ejercicio04 = `SELECT * FROM movies WHERE title LIKE '%til%';`;
 
-const ejercicio05 = '';
+const ejercicio05 = 'SELECT * FROM movies WHERE ARRAY_LENGTH(actors, 1) = 1;';
 
-const ejercicio06 = '';
+const ejercicio06 =
+  'SELECT title, (SELECT AVG(rating_element) FROM UNNEST(ratings) AS rating_element) FROM movies;';
 
-const ejercicio07 = '';
+const ejercicio07 = `SELECT actors FROM movies WHERE title LIKE '%Fast and%' AND year = 2016;`;
 
 module.exports = {
-   ejercicio02,
-   ejercicio03,
-   ejercicio04,
-   ejercicio05,
-   ejercicio06,
-   ejercicio07,
+  ejercicio02,
+  ejercicio03,
+  ejercicio04,
+  ejercicio05,
+  ejercicio06,
+  ejercicio07,
 };
---
+++ FT-M4-master/02-sql/homework/01 - Exercises/movies_hw.session.sql
@@ -0,0 +1,76 @@
+-- 👩‍💻 EJERCICIO 1
+SELECT
+  *
+FROM
+  movies
+WHERE
+  year = 1999;
+
+-- 👩‍💻 EJERCICIO 2
+-- ¿Cómo sería el comando para obtener todas aquellas películas que tengan una duración menor a 90 minutos? En total son 9 películas que cumplen esta condición.
+SELECT
+  *
+FROM
+  movies
+WHERE
+  duration < 90;
+
+-- 👩‍💻 EJERCICIO 3
+--¿Cómo sería el comando para obtener todas aquellas películas creadas entre el año 1930 y 1940? En total son 4 películas que cumplen esta condición.
+SELECT
+  *
+FROM
+  movies
+WHERE
+  year BETWEEN 1930
+  AND 1940;
+
+-- 👩‍💻 EJERCICIO 4
+-- ¿Cómo sería el comando para obtener todas aquellas películas que tengan en su titulo el substring 'til'? En total son 3 películas que cumplen esta condición.
+SELECT
+  *
+FROM
+  movies
+WHERE
+  title LIKE '%til%';
+
+-- 👩‍💻 EJERCICIO 5
+-- ¿Cómo sería el comando para obtener todas aquellas películas que tengan sólo 1 actor en su propiedad actors? En total son 27 películas que cumplen esta condición.
+SELECT
+  *
+FROM
+  movies
+WHERE
+  ARRAY_LENGTH(actors, 1) = 1;
+
+-- 👩‍💻 EJERCICIO 6
+-- ¿Cómo sería el comando para obtener el título de cada película, incluyendo el promedio de todas sus puntuaciones de rating? Recuerda que "rating" es una propiedad de la tabla movies, y es un arreglo de números.
+SELECT
+  title,
+  AVG(rating) AS avg_rating
+FROM
+  movies,
+  UNNEST(ratings) AS rating
+GROUP BY
+  title;
+
+SELECT
+  title,
+  (
+    SELECT
+      AVG(rating_element)
+    FROM
+      UNNEST(ratings) AS rating_element
+  )
+FROM
+  movies;
+
+-- 👩‍💻 EJERCICIO 7
+-- ¿Cómo sería el comando para obtener a los actores de aquellas películas que incluyan en su nombre el string "Fast and", pero que también sean del año 2016? En total serán dos filas con nombres de actores.
+SELECT
+  actors
+FROM
+  movies
+WHERE
+  title LIKE '%Fast and%'
+  AND year = 2016;
