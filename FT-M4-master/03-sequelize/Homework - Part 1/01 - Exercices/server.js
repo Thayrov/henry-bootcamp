21bc8b7926be9509a634f594c8899a69167d2023
thayrov <thayrovg@gmail.com>
2024-01-11 12:35:46 -0600

    💡 [ADD]: Configure Routes
    Set up primary routes in server.js and completed middleware skeletons for Character model.

--- FT-M4-master/03-sequelize/Homework - Part 1/01 - Exercices/server.js
+++ FT-M4-master/03-sequelize/Homework - Part 1/01 - Exercices/server.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const characterMiddleware = require("./middlewares/character.js");
-// const abilityMiddleware = require("./middlewares/ability.js");
+const express = require('express');
+const characterMiddleware = require('./middlewares/character.js');
+const abilityMiddleware = require('./middlewares/ability.js');
 
 const server = express();
 
 server.use(express.json());
 
-server.use("/character", characterMiddleware);
-// server.use("/ability", abilityMiddleware);
+server.use('/character', characterMiddleware);
+server.use('/ability', abilityMiddleware);
 
-server.get("/", (req, res) => {
-  res.send("Henry Sequelize Homework");
+server.get('/', (req, res) => {
+  res.send('Henry Sequelize Homework');
 });
 
 module.exports = server;
